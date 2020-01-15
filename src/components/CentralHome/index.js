c91e5ddb42ab586bcdf37fdf80a1591e9e5f45ee
JayZonday <jmoloughney25@gmail.com>
2020-01-14 19:26:02 -0500

    set up basic file structure

---
+++ src/components/CentralHome/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class CentralHome extends Component {
+
+
+  render(){
+
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default CentralHome;
---
+++ src/components/MainBody/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class MainBody extends Component {
+
+
+  render(){
+
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default MainBody;
