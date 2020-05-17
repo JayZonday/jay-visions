fdf9b76c0b0eae11862780b7d8e3f90b802f4b94
JayZonday <jmoloughney25@gmail.com>
2020-05-17 14:13:50 -0400

    new font in about me new link in demo gratefulcounselinging

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -12,13 +12,13 @@ class AboutMe extends Component {
         <h1 className='about-title'> All About Jay </h1>
         <img className='bio-pic' src={BioPic}/>
         <div className='bio-desc'>
-          Hi! My name is Joseph Moloughney and I'm a web developer. My journey to this day, has been a long and challenging
-          journey - however - I can happily say I'm exactly where I want to be now. I spend most of my days coding away whether it
+          Hello World! I'm Joe Moloughney and I'm a web developer. My journey to this day, has been a long and challenging
+          journey - I can now proudly say I'm exactly where I want to be. I spend most of my days coding away whether it
           be for work or pleasure; You will almost always find me on the web. If not - I also enjoy a nice hikes & walks with my favorite
           pup, Dallas.
           <br/>
           <br/>
-          Enough about me... now lets learn about you and what your web needs are and lets get to work!
+          Enough about me... now lets learn about you and your web needs and lets get to work!
         </div>
       </div>
     );
--- src/components/Demo/index.js
+++ src/components/Demo/index.js
@@ -14,7 +14,7 @@ class Demo extends Component {
           <a className='demo-link' id='portfolio' href='http://www.JosephWilliam.tk' target='_blank'>Portfolio Site</a>
         </div>
         <div className='demo' id='grateful'>
-          <a className='demo-link' id='grateful' href='http://www.GratefulCounseling.tk' target='_blank' >Grateful Counseling Services LLC</a>
+          <a className='demo-link' id='grateful' href='http://www.GratefulCounselingServices.com' target='_blank' >Grateful Counseling Services LLC</a>
         </div>
         <div className='demo' id='gamco'>
           <a className='demo-link' id='gamco' href='http://www.gabelli.com' target='_blank' >GAMCO Investors</a>
