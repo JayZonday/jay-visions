d76717d0a694d35a5e8131be22eb0f2ae0f45208
JayZonday <jmoloughney25@gmail.com>
2020-05-17 10:22:04 -0400

    light blue test

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -14,7 +14,7 @@ class AboutMe extends Component {
         <div className='bio-desc'>
           Hi! My name is Joseph Moloughney and I'm a web developer. My journey to this day, has been a long and challenging
           journey - however - I can happily say I'm exactly where I want to be now. I spend most of my days coding away whether it
-          be for work or pleasure; You're almost certainly find me on the web. If not - I always enjoy a nice walk with my favorite
+          be for work or pleasure; You will almost always find me on the web. If not - I also enjoy a nice hikes & walks with my favorite
           pup, Dallas.
           <br/>
           <br/>
