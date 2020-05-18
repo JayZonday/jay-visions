ef7a0a229ce7b0b1f08685fe083d55583c25c856
JayZonday <jmoloughney25@gmail.com>
2020-05-18 11:08:38 -0400

    fixed demo links entire button is now clickable

--- src/components/Demo/index.js
+++ src/components/Demo/index.js
@@ -10,15 +10,12 @@ class Demo extends Component {
       <div className='demo-section'>
         <h1 className='recent-title'>Recent Works</h1>
 
-        <div className='demo' id='portfolio'>
-          <a className='demo-link' id='portfolio' href='http://www.JosephWilliam.tk' target='_blank'>Portfolio Site</a>
-        </div>
-        <div className='demo' id='grateful'>
-          <a className='demo-link' id='grateful' href='http://www.GratefulCounselingServices.com' target='_blank' >Grateful Counseling Services LLC</a>
-        </div>
-        <div className='demo' id='gamco'>
-          <a className='demo-link' id='gamco' href='http://www.gabelli.com' target='_blank' >GAMCO Investors</a>
-        </div>
+          <a className='demo' id='portfolio' href='http://www.JosephWilliam.tk' target='_blank'>Portfolio Site</a>
+
+          <a className='demo' id='grateful' href='http://www.GratefulCounselingServices.com' target='_blank' >Grateful Counseling Services LLC</a>
+
+          <a className='demo' id='gamco' href='http://www.gabelli.com' target='_blank' >GAMCO Investors</a>
+
       </div>
     );
   }
