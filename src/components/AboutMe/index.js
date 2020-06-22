a74a9cfc58dda4c288a5de586be4510bbc4bdec9
JayZonday <jmoloughney25@gmail.com>
2020-06-22 18:38:02 -0400

    minor changes to display on mobile;

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -12,13 +12,12 @@ class AboutMe extends Component {
         <h1 className='about-title'> All About Jay </h1>
         <img className='bio-pic' src={BioPic}/>
         <div className='bio-desc'>
-          Hello World! I'm Joe Moloughney and I'm a web developer. My journey to this day, has been a long and challenging
-          journey - I can now proudly say I'm exactly where I want to be. I spend most of my days coding away whether it
+          Hello World! I'm Joe Moloughney and I'm a web developer. I proudly say I'm exactly where I want to be. I spend most of my days coding away whether it
           be for work or pleasure; You will almost always find me on the web. If not - I also enjoy a nice hikes & walks with my favorite
           pup, Dallas.
           <br/>
           <br/>
-          Enough about me... now lets learn about you and your web needs and lets get to work!
+          Enough about me... now lets learn about you and your web needs & lets work together!
         </div>
       </div>
     );
--- src/components/Demo/index.js
+++ src/components/Demo/index.js
@@ -10,8 +10,6 @@ class Demo extends Component {
       <div className='demo-section'>
         <h1 className='recent-title'>Recent Works</h1>
 
-          <a className='demo' id='portfolio' href='http://www.JosephWilliam.tk' target='_blank'>Portfolio Site</a>
-
           <a className='demo' id='grateful' href='http://www.GratefulCounselingServices.com' target='_blank' >Grateful Counseling Services LLC</a>
 
           <a className='demo' id='gamco' href='http://www.gabelli.com' target='_blank' >GAMCO Investors</a>
